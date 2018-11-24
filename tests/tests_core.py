94272a33e28c908abd9331fd1fbd69c435b4da70
Nitish Tripathi <nitish.trp1990@gmail.com>
2018-11-24 16:10:11 +0100

    adds unittest for get_input (#3)

--- tests/tests_core.py
+++ tests/tests_core.py
@@ -7,14 +7,70 @@ from apeer_dev_kit import _core
 
 class TestsCore(unittest.TestCase):
 
-    def setUp(self):
+    def test_init_givenNoEnvironmentVariable_coreIsNotInitialized(self):
+        with self.assertRaises(KeyError):
+            _core._core()
+
+    def test_init_givenEmptyInputJson_coreIsInitialized(self):
         os.environ["WFE_INPUT_JSON"] = "{}"
 
-    def test_coreIsInitialized(self):
         core = _core._core()
+
         self.assertTrue(core)
         self.assertEqual(core._wfe_output_params_file, "")
         self.assertEqual(core._outputs, {})
+
+    def test_init_givenInputJson_coreIsInitalized(self):
+        os.environ["WFE_INPUT_JSON"] = '{"WFE_output_params_file":"param.json","red":0.2,"input_image":"test.jpg"}'
+
+        core = _core._core()
+
+        self.assertTrue(core)
+        self.assertEqual(core._input_json["WFE_output_params_file"], "param.json")
+    
+    def test_get_inputs_givenNoParamFileKey_throwsKeyErrorException(self):
+        os.environ["WFE_INPUT_JSON"] = '{"red":0.2,"input_image":"test.jpg"}'
+        core = _core._core()
+
+        with self.assertRaises(KeyError):
+            core._get_inputs()
+
+    def test_get_inputs_givenInputJson_paramFileIsNotPartOfInputs(self):
+        os.environ["WFE_INPUT_JSON"] = '{"WFE_output_params_file":"param.json","red":0.2}'
+        core = _core._core()
+
+        inputs = core._get_inputs()
+        
+        self.assertFalse(hasattr(inputs, 'WFE_output_params_file'))
+        self.assertEqual(len(vars(inputs)), 1)
+
+    def test_get_inputs_givenDecimal_isDeserialized(self):
+        os.environ["WFE_INPUT_JSON"] = '{"WFE_output_params_file":"param.json","red":0.2}'
+        core = _core._core()
+
+        inputs = core._get_inputs()
+        
+        self.assertEqual(inputs.red, 0.2)
+
+    def test_get_inputs_givenInteger_isDeserialized(self):
+        os.environ["WFE_INPUT_JSON"] = '{"WFE_output_params_file":"param.json","red":2}'
+        core = _core._core()
+
+        inputs = core._get_inputs()
+        
+        self.assertEqual(inputs.red, 2)
+
+    def test_get_inputs_givenString_isDeserialized(self):
+        os.environ["WFE_INPUT_JSON"] = '{"WFE_output_params_file":"param.json","value":"testValue"}'
+        core = _core._core()
+
+        inputs = core._get_inputs()
+        
+        self.assertEqual(inputs.value, "testValue")
+
+    def tearDown(self):
+        if "WFE_INPUT_JSON" in os.environ:
+            del os.environ['WFE_INPUT_JSON']
 
 if __name__ == '__main__':
     unittest.main()
