e5997c7a60eba9ff19a8d1f7b2497a50d95d618e
Thomas Irmer <thomas.irmer@rub.de>
2018-11-25 13:01:59 +0100

    Adds README as long description and adds classifiers; Updates to 0.3.0

--- setup.py
+++ setup.py
@@ -1,9 +1,18 @@
 from setuptools import setup
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(name='apeer-dev-kit',
-      version='0.2.0',
+      version='0.3.0',
       description='Development kit for creating modules on apeer',
+      long_description=long_description,
+      long_description_content_type="text/markdown",
       url='https://github.com/apeer-micro/apeer-python-sdk',
       author='apeer-micro',
-      license='MIT',
-      packages=['apeer_dev_kit'])
+      packages=['apeer_dev_kit'],
+      classifiers=[
+        "Programming Language :: Python :: 2",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],)
