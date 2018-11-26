e7e53ab2c68782a43df55225881a18f5c8624743
Thomas Irmer <thomas.irmer@zeiss.com>
2018-11-26 14:36:26 +0100

    Updates to 1.0.3

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setup(name='apeer-dev-kit',
-      version='1.0.2',
+      version='1.0.3',
       description='Development kit for creating modules on apeer',
       long_description=long_description,
       long_description_content_type="text/markdown",
