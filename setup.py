017d1c9252d71c61f15470d2ca2e7985aa49c3a2
Nitish Tripathi <m1ntripa@zeiss.com>
2018-12-21 10:09:55 +0100

    fixes setup

--- setup.py
+++ setup.py
@@ -1,13 +1,8 @@
 from setuptools import setup
 
-with open("README.md", "r") as fh:
-    long_description = fh.read()
-
 setup(name='apeer-dev-kit',
       version='1.0.8',
       description='Development kit for creating modules on apeer',
-      long_description=long_description,
-      long_description_content_type="text/markdown",
       url='https://github.com/apeer-micro/apeer-python-sdk',
       author='apeer-micro',
       packages=['apeer_dev_kit'],
@@ -15,4 +10,10 @@ setup(name='apeer-dev-kit',
         "Programming Language :: Python :: 2",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
-    ],)
+    ],
+    long_description="""
+APEER Python SDK aka (ADK) is a Python library for reading inputs and writing outputs of APEER(https://www.apeer.com) modules. 
+The ADK will take care of reading inputs from previous modules in APEER and writing your outputs in the correct format for the next mod ule.
+This project is hosted at https://github.com/apeer-micro/apeer-python-sdk
+The documentation can be found at https://github.com/apeer-micro/apeer-python-sdk/blob/master/README.md
+""")
