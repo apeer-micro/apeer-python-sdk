79ebff5c805a9c371262f9b5e2af812ce778874a
Thomas Irmer <thomasirmer@gmail.com>
2020-11-12 20:15:11 +0100

    Bumps version to 1.2.0

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='apeer-dev-kit',
-      version='1.1.0',
+      version='1.2.0',
       description='Development kit for creating modules on apeer',
       url='https://github.com/apeer-micro/apeer-python-sdk',
       author='apeer-micro',
@@ -12,7 +12,7 @@ setup(name='apeer-dev-kit',
         "Operating System :: OS Independent",
     ],
     long_description="""
-APEER Python SDK aka (ADK) is a Python library for reading inputs and writing outputs of APEER(https://www.apeer.com) modules. 
+APEER Python SDK aka (ADK) is a Python library for reading inputs and writing outputs of APEER(https://www.apeer.com) modules.
 The ADK will take care of reading inputs from previous modules in APEER and writing your outputs in the correct format for the next mod ule.
 This project is hosted at https://github.com/apeer-micro/apeer-python-sdk
 The documentation can be found at https://github.com/apeer-micro/apeer-python-sdk/blob/master/README.md
